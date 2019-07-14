19c2eb23f66c4cf7e6d56d13fe045c8e6840efd5
Sanjay <sanjay.bajracharya@deepend.com.au>
2019-07-14 17:16:05 +1000

    Add new project to implement package

---
+++ src/Sanz.UUID/App_Plugins/SanzUUIDDatatype/app.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module("umbraco");
+app.controller('sanzUniqueIdController', function ($scope) {
+    var uid = generateUUID();
+    $scope.model.value = uid;
+});
+
+function generateUUID() { 
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+        d += performance.now(); 
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
